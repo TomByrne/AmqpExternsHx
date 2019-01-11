1ca2a68cd26aae7e6c45f08be0c03df465230fdc
Thomas Byrne <tom.byrne@imagination.com>
2019-01-11 16:26:31 +1100

    Added test

---
+++ test/ImportAll.hx
@@ -0,0 +1,17 @@
+package;
+
+import amqp.Amqp;
+import amqp.AmqpBuffer;
+import amqp.AmqpChannel;
+import amqp.AmqpConfirmChannel;
+import amqp.AmqpConnection;
+import amqp.AmqpError;
+import amqp.AmqpMsg;
+
+class ImportAll
+{
+	
+	static function main(){
+		
+	}
+}
