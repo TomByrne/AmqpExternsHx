439abd1e7c2caa462abc9be2605ee211b18184e0
Thomas Byrne <tom.byrne@imagination.com>
2019-03-28 14:45:06 +1100

    v0.0.6 Added fields to AmqpMsgProperties and AmqpMsgFields

--- src/amqp/AmqpMsg.hx
+++ src/amqp/AmqpMsg.hx
@@ -1,5 +1,7 @@
 package amqp;
 
+import haxe.DynamicAccess;
+
 /**
  * ...
  * @author Thomas Byrne
@@ -7,6 +9,33 @@ package amqp;
 typedef AmqpMsg =
 {
 	content: AmqpBuffer,
-	fields: {},
-	properties: {},
+	fields: AmqpMsgFields,
+	properties: AmqpMsgProperties,
+}
+
+typedef AmqpMsgFields =
+{
+    consumerTag: String,
+    deliveryTag: Int,
+    exchange: String,
+    redelivered: Bool,
+    routingKey: String,
+}
+
+typedef AmqpMsgProperties =
+{
+    appId: String,
+    clusterId: String,
+    contentEncoding: String,
+    contentType: String,
+    correlationId: String,
+    deliveryMode: String,
+    expiration: String,
+    headers: DynamicAccess<String>,
+    messageId: String,
+    priority: Dynamic,
+    replyTo: String,
+    timestamp: String,
+    type: String,
+    userId: String,
 }
